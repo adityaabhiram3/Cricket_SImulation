2cd337a10be3afbd570073323792089445fb2ae9
Nilay <email.nilay13@gmail.com>
2017-11-10 22:33:47 +0530

    Fixed

--- inputmodule.py
+++ inputmodule.py
@@ -1,15 +1,19 @@
-<<<<<<< HEAD
 import random
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 overs = int(input("Enter number of overs: "))
 balls = 6*overs
 wicket = 0
+player_names = ['Rohit', 'Dhawan', 'Kohli', 'Jadhav', 'Raina', 'Dhoni', 'Pandya', 'Bhubneswar', 'Bumrah', 'Chahal', 'yadav']
 l = list()
 e=list()
 d=list()
 strike = -1
-
-player = [[], []]
+p1pointer = -1
+p2pointer = 1
+gapcount = 2
+player = list()
+for q in range(11):
+    player.append([])
 overlist = list()
 p1 = 1
 p2 = 2
@@ -20,11 +24,19 @@ for i in range(1,balls+1):
         a = random.randrange(-1, 7)
     if a == -1:
         wicket+=1
-        player.append([])
+        # player.append([])
         if strike == -1:
-        	p1 += 2
+            # print("Player {0} got out".format(p1))
+            p1 += gapcount
+            p1pointer *= -1
+            p2pointer *= -1
+            gapcount = 2
         else:
-        	p2 += 1
+            # print("Player {0} got out".format(p2))
+            p2 += 1
+            gapcount += 1
+        # p1, p2 = p2, p1
+        # strike *= -1
     if wicket == 10:
         break
     if a != -1:
@@ -33,25 +45,27 @@ for i in range(1,balls+1):
         d.append('w')
         print('w', end = " ")
     else:
-    	if strike == -1:
-    		player[p1-1].append(a)
-    	else:
-    		player[p2-1].append(a)
+        if strike == p1pointer:
+            player[p1-1].append(a)  # showing error for some cases
+        else:
+            player[p2-1].append(a)
         # d.append(a)
-    	print(a, end = " ")
+        print(a, end = " ")
+    if (a%2 == 1) and (a != -1):
+        strike *= -1
     if i%6 == 0 :
         e.append(sum(l))
         print("Runs scored this over: ", sum(l))
-        strike -= -1
-        # if strike = 
+        # if a%2 == 0:
+        strike *= -1
         if strike == -1:
-        	print("Player {0} is on strike".format(p1))
+            print("{0} is on strike".format(player_names[p1-1]))
         else:
-        	print("Player {0} is on strike".format(p2))
-        print("Player {0} and Player {1} are playing".format(p1, p2))
+            print("{0} is on strike".format(player_names[p2-1]))
+        print("{0} and {1} are playing".format(player_names[p1-1], player_names[p2-1]))
         l = []
-    if a%2 == 1:
-    	strike -= -1
+    # if a%2 == 1:
+    #     strike *= -1
 # print(player)
 # print(d)
 # print(e)
@@ -59,14 +73,14 @@ count = 1
 print()
 print("_______________________________")
 for f in player:
-	print("PLayer {0} made {1} runs".format(count, sum(f)))
-	count += 1
+    print("{0} scored {1} runs".format(player_names[count-1], sum(f)))
+    count += 1
 print("Total score: ", sum(e))
 print("Wickets: ", wicket)
 
 graph1 = list()
 for k in range(1, overs+1):
-	graph1.append(sum(e[0:k]))
+    graph1.append(sum(e[0:k]))
 print(graph1)
 plt.plot(graph1)
 plt.show()
@@ -74,80 +88,3 @@ print()
 t = range(1, overs+1)
 plt.bar(t, e)
 plt.show()
-=======
-import random
-import matplotlib.pyplot as plt 
-overs = int(input("Enter number of overs: "))
-balls = 6*overs
-wicket = 0
-l = list()
-e=list()
-d=list()
-strike = -1
-
-player = [[], []]
-overlist = list()
-p1 = 1
-p2 = 2
-
-for i in range(1,balls+1):
-    a = random.randrange(-1, 7)
-    while a == 5:
-        a = random.randrange(-1, 7)
-    if a == -1:
-        wicket+=1
-        player.append([])
-        if strike == -1:
-        	p1 += 2
-        else:
-        	p2 += 1
-    if wicket == 10:
-        break
-    if a != -1:
-        l.append(a)
-    if a == -1:
-        d.append('w')
-        print('w', end = " ")
-    else:
-    	if strike == -1:
-    		player[p1-1].append(a)
-    	else:
-    		player[p2-1].append(a)
-        # d.append(a)
-    	print(a, end = " ")
-    if i%6 == 0 :
-        e.append(sum(l))
-        print("Runs scored this over: ", sum(l))
-        strike -= -1
-        # if strike = 
-        if strike == -1:
-        	print("Player {0} is on strike".format(p1))
-        else:
-        	print("Player {0} is on strike".format(p2))
-        print("Player {0} and Player {1} are playing".format(p1, p2))
-        l = []
-    if a%2 == 1:
-    	strike -= -1
-# print(player)
-# print(d)
-# print(e)
-count = 1
-print()
-print("_______________________________")
-for f in player:
-	print("PLayer {0} made {1} runs".format(count, sum(f)))
-	count += 1
-print("Total score: ", sum(e))
-print("Wickets: ", wicket)
-
-graph1 = list()
-for k in range(1, overs+1):
-	graph1.append(sum(e[0:k]))
-print(graph1)
-plt.plot(graph1)
-plt.show()
-print()
-t = range(1, overs+1)
-plt.bar(t, e)
-plt.show()
->>>>>>> a63d23b92b37c56476396863dd06197e539c4357
