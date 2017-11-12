f9ed914199e232204b6f20ec60db99a3f6c6285b
Nilay <email.nilay13@gmail.com>
2017-11-12 10:47:49 +0530

    minor modifications

--- aditya_cric.py
+++ aditya_cric.py
@@ -1,6 +1,9 @@
 import random
+import os
 import matplotlib.pyplot as plt
 import time
+os.system('clear')
+print("                     CRICKET MATCH SCORECARD  ")
 overs = int(input("The total no. of overs :"))
 balls = overs*6
 over_rate = []
@@ -17,11 +20,11 @@ strike = -1
 pitch_end1 = 0
 pitch_end2 = 1
 for i in range(1,balls+1):
-    time.sleep(0.1)
+    time.sleep(0.1)  # time delay
     run = random.randrange(-1,7)
-    while run == 5 :
+    while run == 5 :  # run should not be equal to 5
         run = random.randrange(-1,7)
-    if run == -1 :
+    if run == -1 :  # condition of wicket
         wickets += 1
         score_rate.append('w')
     if run != -1 :
@@ -65,18 +68,22 @@ print('__________________________________')
 print("\nTotal Score: ",total_score)
 print('Wickets:' ,wickets)
 print("Net run rate: ",total_score/overs)
+print()
 for i in range(wickets+1) :
     if i != pitch_end1 and i != pitch_end2 :
         fow.append(team[i])
-    print(team[i],':',sum(individual_runs[i]), '(', len(individual_runs[i]), ')')
+    if len(individual_runs[i]) != 0:
+        print("{0}: {1} ({2})  Strike rate = {3}".format(team[i], sum(individual_runs[i]), len(individual_runs[i]),round((sum(individual_runs[i])/len(individual_runs[i]))*100, 2 )))
 if wickets == 10:
     fow.append(team[i])
 # fow.pop(-1)
+print()
 print("fall of wickets :",fow)
 graph1 = list()
 for k in range(1, overs+1):
     graph1.append(sum(e[0:k]))
 # print(graph1)
+print("__________________________________")
 choice = input("Show graphs? (Y/N) ")
 if (choice == 'Y') or (choice == 'y'):
     plt.plot(graph1)
