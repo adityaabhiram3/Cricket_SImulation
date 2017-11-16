8dcff4dfe235f2d15dded7370c7a0c599ce5194b
Nilay <email.nilay13@gmail.com>
2017-11-16 22:41:38 +0530

    added comments

--- Cricket_nobugs1.py
+++ Cricket_nobugs1.py
@@ -6,39 +6,41 @@ import os
 import matplotlib.pyplot as plt
 import time
 
+# Color codes (Unicode values)
 heading = "\033[1;32;50m{0}\033[00m"
 red="\033[1;37;31m{0}\033[00m"
 blue="\033[1;34;49m{0}\033[00m"
 green="\033[1;32;47m{0}\033[00m"
 magenta="\033[1;35;47m{0}\033[00m"
-os.system('clear')
+
+os.system('clear') # clears the terminal window
 print("\n                           ", end = " ")
 print(heading.format("CRICKET MATCH SCORECARD\n"))
 overs = int(input("The total no. of overs :"))
 balls = overs*6
-over_rate = []
+over_rate = [] # contains runs scored in every over
 over_rate1 = []
 score_rate = []
 score_rate1 = []
 wickets = 0
 wickets1 = 0
-total_score = 0
-total_score1 = 0
+total_score = 0 # Total score of team 1
+total_score1 = 0 #Total score of team 2
 individual_runs = []
 individual_runs1 = []
-fow = []
+fow = [] # fall of wickets
 fow1 = []
-e = list()
+e = list() # has the record for every run scored
 e1 = list()
 team = ['Rohit','Dhawan','Kohli','Jadhav','Raina','Dhoni','Pandya','Bhuveneshwar','Bumrah','Chahal','Kuldeep']
 team1 = ['Guptil','Munro','Williamson','Tailor','Latham','DeGrandhome','Southee','Santner','Bolt','Sodhi','Henry']
 for i in range(12) :
-    individual_runs.append([])
+    individual_runs.append([]) 
 strike = -1
 pitch_end1 = 0
 pitch_end2 = 1
 for i in range(1,balls+1):
-    if pitch_end2 > 10:
+    if pitch_end2 > 10: # if more than 10 wickets fell, it breaks out of the loop
         # pitch_end2 -= 1
         break
     if pitch_end1 > 10:
@@ -74,11 +76,11 @@ for i in range(1,balls+1):
             individual_runs[pitch_end2].append(run)
     if run%2 != 0 and run < 6 :
         strike = strike*(-1)
-    if i%6 == 0 :
-        strike = strike*(-1)
+    if i%6 == 0 : # condition of over completion
+        strike = strike*(-1) # strike rotates after every over
         for q in score_rate:
             if q == 'w':
-                print(red.format(q), end = " ")
+                print(red.format(q), end = " ") # wickets are printed in red
             else:
                 print(q, end =" ")
         print(" runs scored in this over is :-",sum(over_rate))
@@ -101,6 +103,8 @@ print(red.format("\nTotal Score: "),total_score)
 print(red.format('Wickets:' ),wickets)
 print(red.format("Net run rate: "),total_score/overs)
 print()
+
+# For printing stats of every player along with S/R
 if wickets != 10 :
     for i in range(wickets+2) :
         if i != pitch_end1 and i != pitch_end2 :
@@ -118,7 +122,7 @@ print()
 print("fall of wickets :",fow)
 graph1 = list()
 graph1.append(0)
-for k in range(1, overs+1):
+for k in range(1, overs+1): # creates a list to be plotted for graph (values of y = list(function(x)))
     graph1.append(sum(e[0:k]))
 if wickets == 10:
     print("Overs Played", overs_played)
@@ -218,13 +222,14 @@ for k in range(1, overs+1):
     graph2.append(sum(e1[0:k]))
 if wickets1 == 10:
     print("Overs Played", overs_played1)
-if total_score > total_score1:
+if total_score > total_score1: # To check which team won
     winner = 'India'
 else:
     winner = 'New Zealand'
 print("\n\n {0} WON".format(winner))
 
-# print("\n Presentation Ceremony")
+# Highest scoring batsman of the winning team gets the man of the match
+
 if total_score > total_score1:
     highest = 0
     for i in range(len(individual_runs)):
@@ -250,13 +255,11 @@ if (choice == 'Y') or (choice == 'y'):
     # plt.xticks(range(0, overs+1))
     plt.show()
     print()
-    t = np.arange(1, overs+1)
+    t = np.arange(1, overs+1) # creates an array of integers from 1 to number of overs
     width = 0.35
     # if wickets != 10:
-    plt.bar(t, e, width)
+    plt.bar(t, e, width) # for plotting bar graph
     plt.bar(t+width, e1, width)
-    # plt.legend([t, t+width], ['India', 'New Zealand'])
-    # plt.xticks(range(1, overs+1))
     plt.title('Runs per over')
     plt.ylabel('Runs')
     plt.xlabel('Over')
